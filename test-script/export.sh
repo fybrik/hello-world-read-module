413a76b2a7e33b1608e853499f4a90bebeb179c2
nassarmu10 <mohammad.nassar@ibm.com>
2022-01-24 11:21:00 +0200

    Delete unused files.
    Signed-off-by: nassarmu10 <mohammad.nassar@ibm.com>

--- test-script/export.sh
+++
@@ -1,4 +0,0 @@
-export PATH_TO_LOCAL_FYBRIK=/home/mohammadtn/data-movement-operator/fybrik
-export WORKING_DIR=/home/mohammadtn/hello-world-read-module/test-script
-export ACCESS_KEY=1234
-export SECRET_KEY=1234
